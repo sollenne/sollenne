44ea36a254ed3c575b9137116ddf6839fa430461
Sollenne™ <rsollenne@moneygram.com>
2018-09-25 16:28:33 -0500

    add route detection for header links

--- src/app/site.components/header/header.component.ts
+++ src/app/site.components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 
 declare interface IHeaderLink {
   label: string;
@@ -16,11 +17,36 @@ declare interface IHeaderLink {
 export class HeaderComponent implements OnInit {
 
   public headerLinks: Array<IHeaderLink>;
+  public homePage: boolean;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) {
+    this.homePage = true;
+
+    router.events.subscribe( (event: Event) => {
+
+      if (event instanceof NavigationStart) {
+        // something goes here probably... maybe.
+      }
+
+      if (event instanceof NavigationEnd) {
+        if (this.router.url === '/') {
+          this.homePage = true;
+        } else {
+          this.homePage = false;
+        }
+        console.info(this.router.url);
+      }
+
+      if (event instanceof NavigationError) {
+        console.warn(event.error);
+      }
+    });
+
+  }
 
   ngOnInit() {
-
     this.headerLinks = [
       {
         label: '#SOLLENNE',
@@ -49,6 +75,7 @@ export class HeaderComponent implements OnInit {
         url: 'contact',
       },
     ];
+
   }
 
   public action = (label: string): void => {
